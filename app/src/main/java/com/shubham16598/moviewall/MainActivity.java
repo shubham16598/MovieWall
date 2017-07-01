13731de83ad20737981b1ff65a35044b183d3da8
Shubham16598 <shubham16598@gmail.com>
2017-07-01 23:50:25 +0530

    Not Crashing but not working either

--- app/src/main/java/com/shubham16598/moviewall/MainActivity.java
+++ app/src/main/java/com/shubham16598/moviewall/MainActivity.java
@@ -1,10 +1,26 @@
 package com.shubham16598.moviewall;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,15 +32,116 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView = (RecyclerView)findViewById(R.id.recycleView);
-        Data dataretrieve = new Data();
-        try {
-            adapter = new MyCustomAdapter(this,dataretrieve.getData());
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        recyclerView.setAdapter(adapter);
+        Fetchdata datastore = new Fetchdata();
+        datastore.execute();
         recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+
     }
+
+    public final static String BASE_URL ="https://api.themoviedb.org";
+    public final static String PATH ="3/movie";
+    public final static String SORT ="popular";
+    public final static String API_KEY = "05947c19dcc1963b0c0e6fcb66a32aef";
+
+
+    public static URL buildUrl() {
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("https")
+                .authority(BASE_URL)
+                .appendPath(PATH)
+                .appendPath(SORT)
+                .appendQueryParameter("api_key", API_KEY);
+
+        URL url = null;
+        try {
+            url = new URL(builder.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            Log.e("buildUrl: ","url problem" ,e);
+        }
+
+        return url;
+    }
+
+
+
+    public class Fetchdata extends AsyncTask<Object, Object, ArrayList<Information>> {
+        StringBuilder jsonString = new StringBuilder();
+        HttpURLConnection conn;
+        public String BASE_POSTER_URL = " http://image.tmdb.org/t/p/w185/";
+        @Override
+        protected ArrayList<Information> doInBackground(Object... voids) {
+            try {
+
+                // Setup HttpURLConnection class to send and receive data from php and mysql
+                conn = (HttpURLConnection) buildUrl().openConnection();
+                conn.setReadTimeout(15000);
+                conn.setConnectTimeout(10000);
+                conn.setRequestMethod("GET");
+
+                // setDoOutput to true as we recieve data from json file
+                conn.setDoOutput(true);
+
+            } catch (IOException e1) {
+
+                e1.printStackTrace();
+                Log.e("doInBackground: ","Connection problem" );
+            }
+
+            try {
+
+                int response_code = conn.getResponseCode();
+
+                // Check if successful connection made
+                if (response_code == HttpURLConnection.HTTP_OK) {
+
+                    // Read data sent from server
+                    InputStream input = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        jsonString.append(line);
+                    }
+
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e("doInBackground: ","Response error" );
+            } finally {
+                conn.disconnect();
+            }
+
+
+            ArrayList<Information> data = new ArrayList<>();
+            try {
+
+                JSONObject baseJsonObject = new JSONObject(String.valueOf(jsonString));
+                JSONArray result = baseJsonObject.getJSONArray("results");
+                for (int i = 0;i<result.length();i++){
+                    JSONObject currentMovie = result.getJSONObject(i);
+                    String posterPath = currentMovie.getString("poster_path");
+                    Information current = new Information();
+                    current.imageID = BASE_POSTER_URL+posterPath;
+                    data.add(current);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.e("Data", "Problem parsing the posters movies", e);
+            }
+
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Information> data) {
+            super.onPostExecute(data);
+            adapter = new MyCustomAdapter(getApplicationContext(),data);
+            recyclerView.setAdapter(adapter);
+
+        }
+    }
+
 }
